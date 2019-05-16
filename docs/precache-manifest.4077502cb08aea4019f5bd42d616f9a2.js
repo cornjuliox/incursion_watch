435528f9d2b9bf3fbf266bff4a35cd7fc68a3236
Enrico Tuvera Jr <enricojr.tuvera@gmail.com>
2019-05-17 02:31:27 +0800

    updated homepomepage key in package.json to fix homepage

---
+++ docs/precache-manifest.4077502cb08aea4019f5bd42d616f9a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "147c92325eb7acacb78a2f485e9a2d2f",
+    "url": "/incursion_watch/static/media/fruits-watermelon.147c9232.gif"
+  },
+  {
+    "revision": "eb41adcf2153a297bb71",
+    "url": "/incursion_watch/static/js/runtime~main.eb41adcf.js"
+  },
+  {
+    "revision": "53dc250dbb3140076005",
+    "url": "/incursion_watch/static/js/main.53dc250d.chunk.js"
+  },
+  {
+    "revision": "35a393a602e546d7568e",
+    "url": "/incursion_watch/static/js/1.35a393a6.chunk.js"
+  },
+  {
+    "revision": "53dc250dbb3140076005",
+    "url": "/incursion_watch/static/css/main.143d6b56.chunk.css"
+  },
+  {
+    "revision": "35a393a602e546d7568e",
+    "url": "/incursion_watch/static/css/1.ef9216de.chunk.css"
+  },
+  {
+    "revision": "a3f074eddea0c4fdd9a579e707fb26c8",
+    "url": "/incursion_watch/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8de25a0ec362eeb0783d20a06988a113.js"
+  "/incursion_watch/precache-manifest.4077502cb08aea4019f5bd42d616f9a2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/incursion_watch/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
